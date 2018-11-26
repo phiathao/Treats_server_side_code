658af04682db7961cd4db433b06a4b177e73cfe9
phiathao <phiat11@gmail.com>
2018-11-26 13:13:39 -0600

    get working

--- server/routes/treats.router.js
+++ server/routes/treats.router.js
@@ -3,11 +3,26 @@ const pool = require('../modules/pool');
 
 
 // GET /treats
-
+router.get('/', (req, res)=>{
+    console.log('in Get')
+    let qString = `SELECT * FROM treats ORDER BY id ASC;`;
+    pool.query(qString).then((result) => {
+        res.send(result.rows);
+    }).catch((err) => {
+        console.log(err);
+        res.sendStatus(500);
+    });
+});
 // POST /treats
-
+router.post('/', (req, res)=>{
+    console.log('in Get')
+});
 // PUT /treats/<id>
-
+router.put('/', (req, res)=>{
+    console.log('in Get')
+});
 // DELETE /treats/<id>
-
+router.delete('/', (req, res)=>{
+    console.log('in Get')
+});
 module.exports = router;
